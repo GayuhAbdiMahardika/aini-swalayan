097e64cff0510a9a158d538872a4c325997d76cb
AndikaYudaSetyawan <yudascoppy@gmail.com>
2024-12-04 16:14:25 +0700

    web-php

--- aini-swalayan/routes/web.php
+++ aini-swalayan/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SupplierController;
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
@@ -12,4 +13,13 @@ Route::group(['middleware' => ['auth:data_user']], function () {
     Route::get('/', function () {
         return view('admin');
     });
-});
+
+    Route::get('/supplier', [SupplierController::class, 'tampil'])->name('supplier.tampil');
+    Route::get('/supplier/tambah', [SupplierController::class, 'tambah'])->name('supplier.tambah');
+    Route::post('/supplier/submit', [SupplierController::class, 'submit'])->name('supplier.submit');
+    Route::get('/supplier/edit/{id}', [SupplierController::class, 'edit'])->name('supplier.edit');
+    Route::post('/supplier/update/{id}', [SupplierController::class, 'update'])->name('supplier.update');
+    Route::post('/supplier/delete/{id}', [SupplierController::class, 'delete'])->name('supplier.delete');
+
+    });
+
